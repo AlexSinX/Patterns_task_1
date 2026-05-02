1ef2cfbe57129bc16ad1b120a3fe5ddd282f434b
AlexSinX <alexsin1712@yandex.ru>
2026-05-02 22:35:08 +0300

    Add Test

--- src/test/java/ru/netology/DeliveryTest.java
+++ src/test/java/ru/netology/DeliveryTest.java
@@ -1,8 +1,11 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.delivery.data.DataGenerator;
+
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryTest {
@@ -15,8 +18,31 @@ public class DeliveryTest {
     }
 
     @Test
-    void shouldSuccessPlanDelivery() {
+    void shouldPlanAndRescheduleDelivery() throws InterruptedException {
         String firstDate = DataGenerator.generateDate(3);
         String secondDate = DataGenerator.generateDate(7);
+
+        // Планирование
+        $("[data-test-id=city] input").setValue(userInfo.getCity());
+        $("[data-test-id=date] input").doubleClick().sendKeys(org.openqa.selenium.Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstDate);
+        $("[data-test-id=name] input").setValue(userInfo.getName());
+        $("[data-test-id=phone] input").setValue(userInfo.getPhone());
+        $("[data-test-id=agreement]").click();
+        $$("button").findBy(Condition.exactText("Забронировать")).click();
+
+        Thread.sleep(2000);
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstDate));
+        $("[data-test-id=success-notification] .notification__closer").click();
+
+        // Перепланирование
+        $("[data-test-id=date] input").doubleClick().sendKeys(org.openqa.selenium.Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(secondDate);
+        $$("button").findBy(Condition.exactText("Забронировать")).click();
+
+        Thread.sleep(2000);
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondDate));
     }
 }
